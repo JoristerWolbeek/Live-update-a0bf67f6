8000d09d17a196f2fa8780213d1cbb1b3ef12829
Joris ter Wolbeek <21074092@talnet.nl>
2020-03-09 15:26:37 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const pi = 3.14;
 
+window.setInterval(time, 1000);
+
+function time(){
+    var d = new Date();
+    document.getElementById("time").innerHTML = d;
+}
+
 function bereken(){
     let diameter;
     diameter = document.getElementById("diameter").value;
